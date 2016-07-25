f05e05e699ab02dfe75a59e2292e177622083a22
Evgeni Gurvitch <evgeni.gurvitch@ironsrc.com>
2016-07-25 17:28:55 +0300

    added concurrency to the test

--- test.go
+++ test.go
@@ -5,33 +5,52 @@ import (
 	"fmt"
 	"github.com/streamrail/uap-go/uaparser"
 	"os"
+	"strconv"
 	"time"
+	"sync"
 	netUtil "github.com/streamrail/common/util/net"
 )
 
 func main() {
-	if len(os.Args) < 2 {
-		fmt.Printf("Usage: %s [old|new|both]\n", os.Args[0])
+	if len(os.Args) < 3 {
+		fmt.Printf("Usage: %s [old|new|both] [concurrency level]\n", os.Args[0])
 		return
 	}
+	var wg sync.WaitGroup
+	cLevel, _ := strconv.Atoi(os.Args[2])
 	switch (os.Args[1]) {
 		case "new":
 			fmt.Printf("Running new version of uap...")
 			uaParser, _ := uaparser.NewWithOptions("/etc/regexes.yaml", (uaparser.EOsLookUpMode | uaparser.EUserAgentLookUpMode), 100, 20, true)
-			runTest(uaParser)
+			for i := 0; i < cLevel; i++ {
+				wg.Add(1)
+				go runTest(uaParser, i, &wg)
+			}
+			wg.Wait()
 			return
 		case "old":
 			fmt.Printf("Running old version of uap...")
 			uaParser, _ := uaparser.New("/etc/regexes.yaml")
-			runTest(uaParser)
+			for i := 0; i < cLevel; i++ {
+				wg.Add(1)
+				go runTest(uaParser, 0, &wg)
+			}
+			wg.Wait()
 			return
 		case "both":
 			fmt.Printf("Running new version of uap...")
 			uaParser, _ := uaparser.NewWithOptions("/etc/regexes.yaml", (uaparser.EOsLookUpMode | uaparser.EUserAgentLookUpMode), 100, 20, true)
-			runTest(uaParser)
+			for i := 0; i < cLevel; i++ {
+				wg.Add(1)
+				runTest(uaParser, 0i, &wg)
+			}
 			fmt.Printf("Running old version of uap...")
 			uaParser, _ = uaparser.New("/etc/regexes.yaml")
-			runTest(uaParser)
+			for i := 0; i < cLevel; i++ {
+				wg.Add(1)
+				runTest(uaParser, 0, &wg)
+			}
+			wg.Wait()
 			return
 		default:
 			fmt.Printf("Usage: %s [old|new|both]\n", os.Args[0])
@@ -40,7 +59,7 @@ func main() {
 }
 
 
-func runTest(uaParser *uaparser.Parser) {
+func runTest(uaParser *uaparser.Parser, id int, wg *sync.WaitGroup) {
 	file, err := os.Open("./uas")
 	if err != nil {
 		fmt.Printf("Failed to open ./wrappers file. Error: %s\n", err.Error())
@@ -48,6 +67,10 @@ func runTest(uaParser *uaparser.Parser) {
 	}
 	defer file.Close()
 	line := 0
+	delim := ""
+	for i:= 0; i < id * 2; i++ {
+		delim += "\t"
+	}
 	totalLines := countLines()
 	platforms := map[string]int{"mobile": 0, "desktop": 0}
 	var totalTime time.Duration
@@ -55,6 +78,9 @@ func runTest(uaParser *uaparser.Parser) {
 	for scanner.Scan() {
 		str := scanner.Text()
 		line++
+		if line == 5000 {
+			break
+		}
 		start := time.Now()
 		if netUtil.IsMobileUA(str, uaParser.Parse(str)) {
 			platforms["mobile"]++
@@ -63,9 +89,10 @@ func runTest(uaParser *uaparser.Parser) {
 		}
 		elapsed := time.Since(start)
 		totalTime += elapsed
-		fmt.Printf("\r\t\t\t\t%.2f%% completed", float64(line * 100)/float64(totalLines))
+		fmt.Printf("\r\t\t\t\t%s%.2f%% completed|", delim, float64(line * 100)/float64(totalLines))
 	}
 	fmt.Printf("\nProcessed lines: %d. Test took %s\nResult: %+v\n", line, totalTime, platforms)
+	wg.Done()
 }
 
 func countLines() (int) {
@@ -75,6 +102,9 @@ func countLines() (int) {
 	lineCount := 0
 	for fileScanner.Scan() {
 		lineCount++
+		if lineCount == 5000 {
+			break
+		}
 	}
 	return lineCount
 }
